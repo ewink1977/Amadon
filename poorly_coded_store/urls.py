471e1ff56b3231718d111971a0133a3cdee7fdea
Erin Winking <ewink@yahoo.com>
2020-12-28 22:30:57 -0800

    Changes made to prevent user from setting their own price.

--- poorly_coded_store/urls.py
+++ poorly_coded_store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('checkout/', views.checkout)
+    path('checkout/', views.checkout, name="checkout"),
+    path('thanks/', views.thanks, name="thanks"),
 ]
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -9,20 +9,25 @@ def index(request):
 
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
+    product_data = Product.objects.get(id=request.POST["id"])
+    price_data = float(product_data.price)
+    total_charge = quantity_from_form * price_data
+    print(f"Charging credit card ${ total_charge }")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    return redirect("thanks")
+
+def thanks(request):
+    total_charge = Order.objects.last()
     orders_temp = Order.objects.all()
     all_orders = 0
     for orders in orders_temp:
         all_orders += 1
     orders_sum = 0
     for orders in orders_temp:
-        orders_sum += orders.total_price 
+        orders_sum += orders.total_price
     context = {
         "orderinfo" : total_charge,
         "ordercount" : all_orders, 
         "ordertotal" : orders_sum,
     }
-    return render(request, "store/checkout.html", context)
+    return render(request, 'store/checkout.html', context)
